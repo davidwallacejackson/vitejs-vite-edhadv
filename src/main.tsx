6a8382a0fac0fee281fb71b4275a092c0d1750e8
David Jackson <davidjackson@Davids-MacBook-Pro.local>
2022-09-02 16:33:47 -0700

    comments

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-const registration = navigator.serviceWorker.register('/sw/sw.js');
+const registration = navigator.serviceWorker
+  .register("/sw.js", { scope: "/sw" })
+  .then((registration) => {
+    fetch("/sw/foo");
+    fetch("https://google.com/");
+  });
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+// you'll see fetch events for both /sw/foo and https://google.com/ in the console
+// (though of course the actual fetches will fail)
+
+// switch to /sw/sw.js and you see neither (I'd expect /sw/foo at least to remain)
+
+// alternately, switch to /sw.js with {scope: '/sw'} and you still see neither
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
